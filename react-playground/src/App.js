ccc5e5193de22c3fb0510a32c684fd5ef5e7d38a
zkhin <arandomname@gmail.com>
2019-10-07 17:20:34 -0400

    trello testing

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+// import Card from './composition/Card'
+import List from './composition/List'
 
-function App() {
+function App(props) {
+  let lists = props.store.lists;
+  console.log(lists);
+  let allCards = props.store.allCards;
+
+  let Lists = lists.map((list) => {
+    let cards = [];
+    list.cardIds.forEach(id => {
+      cards.push(allCards[id])
+    })
+    return (<List key={list.id} header={list.header} cards={cards} />)
+  })
+
+
   return (
-    <main className='App'>
-      {/* content goes here */}
+    <main class="App">
+      <header class="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div class="App-list">
+        {Lists}
+      </div>
     </main>
   );
 }
---
+++ react-playground/src/composition/List.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from './Card'
+
+class List extends React.Component{
+    render() {
+
+        let Cards = this.props.cards.map((card, i) => { 
+            return <Card key={i} message={card.message} />
+        })
+        return (
+            <section className="List">
+                <header className="List-header">
+                    <h2>{this.props.header}</h2>
+                </header>
+                <div className="List-cards">
+                    {Cards}
+                </div>
+            </section>
+        )}
+}
+
+export default List;
